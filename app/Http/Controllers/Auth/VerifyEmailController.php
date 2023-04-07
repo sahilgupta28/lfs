6e1593fa8b0651aa56d36e11d4b633024a691335
JD Singh <jagdeepsingh@founderandlightning.com>
2023-04-07 22:32:33 +0530

    Verify email by programmatically login-using-id + logging-out

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -4,18 +4,24 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Auth;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
 
 class VerifyEmailController extends Controller
 {
+    public function __construct()
+    {
+        Auth::loginUsingId(request()->route('id'));
+    }
     /**
      * Mark the authenticated user's email address as verified.
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
+            Auth::logout();
             return redirect()->intended(
                 config('app.frontend_url') . RouteServiceProvider::HOME . '?verified=1'
             );
@@ -25,6 +31,7 @@ class VerifyEmailController extends Controller
             event(new Verified($request->user()));
         }
 
+        Auth::logout();
         return redirect()->intended(
             config('app.frontend_url') . RouteServiceProvider::HOME . '?verified=1'
         );
